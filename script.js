61a781f7768c602a50225ea04a0db79c910da8fa
João Pedro Carvalho <pedromaltapedrozo@gmail.com>
2019-12-18 01:24:43 -0300

    colisao com as paredes

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 canvas = document.getElementById("canvas")
 context = canvas.getContext('2d')
 context.scale(10, 10)
-
 snake = [
-    [0, 1],
-    [0, 2],
-    [0, 3],
-    [1, 3],
+    [1, 6],
+    [1, 5],
     [1, 4],
+    [1, 3],
+    [1, 2],
 ]
-    direction = [0, 0]
+
+direction = [0, 0]
 
 document.addEventListener('keypress', move)
 
 function move(e) {
     var code = e.which || e.keyCode;
     console.log(code)
-    if (code == '87') { 
+    if (code == '87') {
         direction = [0, -1]
         updateLoop()
     }
@@ -34,10 +34,16 @@ function move(e) {
     } 
 }
 
+function verifymove() {
+    head = snake[0]
+    if(head[0] > 49 || head[0] < 0 || head[1] > 49 || head[1] < 0){
+        gameOver()
+    }
+}
 
-
-function gameover() {
+function gameOver() {
     document.removeEventListener('keypress', move)
+    window.alert("Game Over")
 }
 
 function updateLoop() {
@@ -46,7 +52,9 @@ function updateLoop() {
     tail[0] = head[0] + direction[0]
     tail[1] = head[1] + direction[1]
     snake.unshift(tail)
+    verifymove()
     draw()
+    console.log(head)
 }
 
 function draw() {
