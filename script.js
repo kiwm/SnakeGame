41196bcff4e6363284c7b0b093627d2836e6596b
João Pedro Carvalho <pedromaltapedrozo@gmail.com>
2019-12-22 00:28:28 -0300

    sem bugs

--- script.js
+++ script.js
@@ -14,30 +14,35 @@ snake = [
 direction = [1, 0]
 food = [5, 5]
 score = 0
-
+marcador = false
+ var handle
 function start() {
     document.getElementById('btn1').style.display = "none"
     document.getElementById('div1').style.display = "block"
-    document.addEventListener('keypress', move)
-    var handle = setInterval(updateLoop, 60)
-    setTimeout(move, 60) 
-}
+    document.addEventListener('keydown', move)
+    handle = setInterval(updateLoop, 60)
+} 
 
 function move(e) {
-    var code = e.which || e.keyCode;
-    if (code == '119' && !(direction[0] == 0 && direction[1] == 1)) { //cima
-        direction = [0, -1]       
+    console.log(marcador)
+    if(marcador == true) {
+        var code = e.keyCode;
+        if (code == '87' && !(direction[1] == 1)) { //cima
+            direction = [0, -1]    
+        }
+        else if (code == '83' && !(direction[1] == -1)) { //baixo
+            direction = [0, 1]             
+        }
+        else if (code == '65' && !(direction[0] == 1)) { //esquerda
+            direction = [-1, 0] 
+        }
+        else if (code == '68' && !(direction[0] == -1)) { //direita
+            direction = [1, 0] 
+        }
+        marcador = false
     }
-    else if (code == '115' && !(direction[0] == 0 && direction[1] == -1)) { //baixo
-        direction = [0, 1]  
-    }
-    else if (code == '97' && !(direction[0] == 1 && direction[1] == 0)) { //esquerda
-        direction = [-1, 0] 
-    }
-    else if (code == '100' && !(direction[0] == -1 && direction[1] == 0)) { //direita
-        direction = [1, 0]  
-    } 
 }
+
 
 function verifymove() {
     head = snake[0]
@@ -52,12 +57,11 @@ function verifymove() {
 }
 
 function gameOver() {
-    document.removeEventListener('keypress', move)
     text = document.getElementById('score')
     text.innerText = 'Game Over! \n Score: ' + score
     document.getElementById('btn2').style.display = "block"
-    direction = [0, 0]
     clearInterval(handle)
+    document.removeEventListener('keydown', move)
     
 }
 
@@ -68,9 +72,10 @@ function replay() {
 function updateLoop() {
     tail = snake.pop()
     head = snake[0]
-    tail[0] = head[0] + direction[0]
-    tail[1] = head[1] + direction[1]
-    snake.unshift(tail)
+    snake.unshift([
+        snake[0][0] + direction[0],
+        snake[0][1] + direction[1]
+    ])
     if(head[0] == food[0] && head[1] == food[1] || head[0] == food[0] && head[1] == food[1] + 1 || head[0] == food[0] + 1 && head[1] == food[1] || head[0] == food[0] + 1 && head[1] == food[1] + 1) {
         food = [(Math.random() * 40) | 0 , (Math.random() * 40) | 0]
         snake.push([-19, -19])
@@ -79,16 +84,23 @@ function updateLoop() {
     }
     verifymove()
     draw()
+    marcador = true
 }
 
 function draw() {
     context.clearRect(0, 0, 500, 500)
     var img = document.getElementById('food')
+    var headD = document.getElementById('headD')
     context.drawImage(img, food[0], food[1], 2, 2)
     context.fillStyle = 'green'
-    snake.forEach(function([x, y]) {
-        context.fillRect(x, y, 1, 1)  
-    })
+
+    for(i = 0; i < snake.length; i++) {
+        if (i == 0) {
+            context.drawImage(headD, snake[i][0], snake[i][1], 1, 1)
+        } else{
+            context.fillRect(snake[i][0], snake[i][1], 1, 1)
+        }     
+    }
 }
 
 
